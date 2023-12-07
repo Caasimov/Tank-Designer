619869be2c3bfa679d9c01304818f58e2c14a941
Adam Moec <adam.moec@icloud.com>
2023-12-07 15:34:40 +0100

    Defined data flow and iteration logic

--- code/main.py
+++ code/main.py
@@ -1,11 +1,29 @@
+import json
 import utils
 
 # Load JSON file
+with open('./data/materials.json', 'r') as fname:
+    material_types = json.load(fname)
 
-# Declare constants
+# Initialize variables
+var_history = {}
 
 
 # Recursion logic
 
+for material_name, material_properties in material_types.items():
+
+    var_history_buffer = [] # Reset the variable history buffer for the specific material under consideration
+    num_iterations = 0
+    
+    while num_iterations <= 1000: #NOTE: REPLACE WITH ACTUAL CONDITION
+        var_temp_buffer = [] # Reset the calculated design variable values
+        
+        
+        var_history_buffer.append(var_temp_buffer)
+        num_iterations += 1
+    
+    
+    var_history[material_name] = var_history_buffer # Store iteration history in a material-specific dictionary
 
 # Graphical output
